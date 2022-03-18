bead4de4d941490facdca5a5263dbfb7d2e6c330
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-18 11:45:46 -0400

    commit tree 22fb25e4587203c853da9f1fcc71f450460373da

--- index.js
+++ index.js
@@ -11,6 +11,51 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(str => {
+    const splitStr = str.split(" ")
+    const newStr = splitStr.map(word => {return word.charAt(0).toUpperCase() + word.slice(1)})
+    return newStr.join(' ')
+  })
 }
+titleCased();
+
+console.log(tutorials)
+console.log(titleCased())
+
+
+
+
+
+
+
+
+
+
+
+
+  // for(i=0; i < tutorials.length; i++) {
+    //const arr = []  
+       
+      //console.log(tutorials[i].split(" ").length)
+  //}
+ //}
+
+//console.log(titleCased(tutorials))
+
+
+  //return element[0].charAt(0).toUpperCase() + element.substring(1).toLowerCase()}
+//)
+
+// const titleCased = (tutTitle => {
+//   return tutTitle.charAt(0).toUpperCase() + tutTitle.substring(1).toLowerCase();
+// })
+
+// console.log(titleCased)
+
+
+
+// function titleCased(questions) {
+//   for (const of )
+//   return tutorials.map 
+// }
